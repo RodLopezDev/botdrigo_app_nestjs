82c845c8f83f565198df0163add3858d98db0040
RodLopezDev <rodrigolopezdev@gmail.com>
2026-07-03 18:42:21 -0500

    feat(app @RodLopezDev) ✨ Config TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { dataSourceOptions } from './config/data-source';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(dataSourceOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', unique: true })
+  email: string;
+
+  @Column({ name: 'tenant_id', type: 'uuid' })
+  tenantId: string;
+}
