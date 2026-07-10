1b6b294b04b5d4e78dfab2e686eb0e6845295eca
RodLopezDev <rodrigolopezdev@gmail.com>
2026-07-10 12:27:50 -0500

    feat(deploy @RodLopezDev) ✨ Config in order to run for deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
-import { dataSourceOptions } from './config/data-source';
+// import { dataSourceOptions } from './config/data-source';
 
-import { AuthModule } from './modules/auth/auth.module';
-import { TenantModule } from './modules/tenant/tenant.module';
+// import { AuthModule } from './modules/auth/auth.module';
+// import { TenantModule } from './modules/tenant/tenant.module';
+// import { CatalogModule } from './modules/catalog/catalog.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(dataSourceOptions),
-    AuthModule,
-    TenantModule,
+    // TypeOrmModule.forRoot(dataSourceOptions),
+    // AuthModule,
+    // TenantModule,
+    // CatalogModule,
   ],
   controllers: [AppController],
   providers: [AppService],
